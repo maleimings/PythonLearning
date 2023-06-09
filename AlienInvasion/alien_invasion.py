0a3c53aadd1c4ec68acebc62dc786188de8da468
Maleiming <maleimings@gmail.com>
2023-06-09 17:48:31 +0800

    handle bullet alien collisions

--- AlienInvasion/alien_invasion.py
+++ AlienInvasion/alien_invasion.py
@@ -38,6 +38,14 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
 
+        self._check_bullet_alien_collisions()
+
+    def _check_bullet_alien_collisions(self):
+        collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+        if not self.aliens:
+            self.bullets.empty()
+            self._create_fleet()
+
     def _check_events(self):
         """check events"""
         for event in pygame.event.get():
